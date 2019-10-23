d77229656691ba83adbc5cceb23672969bac8ce4
student <skahvedzic1@etf.unsa.ba>
2019-10-23 15:39:07 +0200

    Final Commit

--- Test/ba/unsa/etf/rpr/SupermarketTest.java
+++ Test/ba/unsa/etf/rpr/SupermarketTest.java
@@ -7,12 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class SupermarketTest {
 
     @Test
-    void name() {
-        Artikl artikl=new Artikl("2",1,"2")
+    void ArtiklTest() {
+        Artikl artikl=new Artikl("2",1,"2");
         assertAll ( "GETERI",
                 () -> assertEquals( 1, artikl.getCijena() ),
                 () -> assertEquals( "2", artikl.getKod() )
         );
     }
+
 }
 
